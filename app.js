009399378ffc32017bba000a3d2fd65a52035327
Bunlong Thay <bunlong.thay@nab.com.au>
2018-12-12 13:43:52 +1100

    set up Grunt, Start Script and  Routes Example

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
+
+var usersRouter = require('./routes/users');
+app.use(express.json());
+app.use('/users', usersRouter);
+
+
+/*app.get('/', function(req, res) {
   res.send({
     "Output": "Hello Bunlong!"
   });
@@ -11,8 +17,25 @@ app.post('/', function(req, res) {
   res.send({
     "Output": "Hello World!"
   });
+});*/
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
-module.exports = app
+module.exports = app;
